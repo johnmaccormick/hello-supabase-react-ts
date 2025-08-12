72f92712d926daa736c37f314ab0e29ca104a947
MacCormick <jmac@dickinson.edu>
2025-08-12 11:53:07 -0400

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,64 @@
-import React from 'react'
+// import React from 'react'
 
-export default function App() {
-  return <h1>Hello, TypeScript + React + Vite!</h1>
+// export default function App() {
+//   return <h1>Hello, TypeScript + React + Vite!</h1>
+// }
+
+import { useState, useEffect } from "react";
+import supabaseClient from "./utils/supabase";
+
+function Page() {
+  const [todos, setTodos] = useState<any[]>([]);
+  const [instruments, setInstruments] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function getTodos() {
+      const { data: todos, error } = await supabaseClient
+        .from("todos")
+        .select();
+
+      if (error) {
+        console.error("Error fetching todos:", error);
+        return;
+      }
+
+      if (todos && todos.length > 0) {
+        setTodos(todos);
+      }
+    }
+
+    getTodos();
+  }, []);
+
+  useEffect(() => {
+    async function getInstruments() {
+      const { data: instruments, error } = await supabaseClient
+        .from("instruments")
+        .select();
+
+      if (instruments && instruments.length > 0) {
+        setInstruments(instruments);
+      }
+    }
+
+    getInstruments();
+  }, []);
+
+  return (
+    <>
+      <p>Todos:</p>
+      <div>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </div>
+      <p>Instruments:</p>
+      <div>
+        {instruments.map((instruments) => (
+          <li key={instruments.id}>{instruments.name}</li>
+        ))}
+      </div>
+    </>
+  );
 }
+export default Page;
